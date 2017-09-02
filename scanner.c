7cefa54bd9325939f0d205d6a0239d63907d01fc
Esteban Bosques <nano.95.eb@gmail.com>
2017-09-01 22:30:36 -0600

    Primer commit

---
+++ scanner.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "token.h"
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+
+char *names[] = {NULL, "CONSTANT", "ID", "SEMICOLON", "KEYWORD", "L_BRACKET", 
+					"R_BRACKET", "L_PARENT", "R_PARENT"};
+
+int main(void) 
+{
+	int token;
+
+	token = yylex();
+	while(token) 
+	{
+		printf("%s is set to %s\n", names[token], yytext);
+		token = yylex();
+	}
+	return 0;
+}
---
+++ token.h
@@ -0,0 +1,8 @@
+#define	CONSTANT 1
+#define ID 2
+#define SEMICOLON 3
+#define KEYWORD 4
+#define L_BRACKET 5
+#define R_BRACKET 6
+#define L_PARENT 7
+#define R_PARENT 8
